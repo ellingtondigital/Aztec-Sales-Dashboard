3a7581b75bf117b6c0aa9e29ec4b4f94950ecbad
pasidhu naveen <pasidhu878@gmail.com>
2024-09-12 16:02:49 +0530

    db config

--- backend/config.js
+++ backend/config.js
@@ -8,7 +8,7 @@ module.exports = {
     //   database: 'xldashboard'
     // }
 
-    // baseUrl: 'http://167.99.120.218:5000',
+    baseUrl: 'http://167.99.120.218:5000',
     mysql: {
         port: 3306,
         host: '167.99.120.218',
